9e5b921fd203096af31f7fef3d2c46094f016bbe
Tarun Gattu <gttarun@taruns-mbp.attlocal.net>
2019-11-07 16:21:34 -0600

    modified parks model to include more details/fields

--- parks/models.py
+++ parks/models.py
@@ -9,6 +9,9 @@ class Park(models.Model):
 	name = models.CharField(max_length=50)
 	longitude = models.DecimalField(max_digits=9, decimal_places=6)
 	latitude = models.DecimalField(max_digits=9, decimal_places=6)
+	dog_friendly = models.BooleanField(default=False)
+	details = models.CharField(max_length=250, null=True)
+
 
 	def __str__(self):
 		return self.name
